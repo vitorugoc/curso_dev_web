7538ea00582916718b5287e0ac26894ba52331d2
vitorugoc <vitorhugo02cc@gmail.com>
2023-02-11 18:26:58 -0300

    aula chaveValor

---
+++ fundamentos-js/chaveValor.js
@@ -0,0 +1,17 @@
+const saudacao = "Olá"
+return saudacao
+}
+
+console.log(saudacao)
+console.log(saudar())
+
+const aluno = {
+'nome': 'Fulano',
+'idade': 30,
+'materia': "Matemática",
+'novoObj' : {
+  'blalab': 'blababa'
+}
+}
+
+console.log(aluno)
